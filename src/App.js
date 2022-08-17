07f6b6b64a1b0d502b2cf95c8d84c40d9f43de4e
Rishabh Dwivedi <rishabhdwivedi2002@gmail.com>
2022-08-17 16:31:56 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Data from "./Data";
+export default function App()
+{
+  const [mySet, setState] = React.useState(Data)
+  const [currQues, setQues] = React.useState(0)
+  const [counter, setCounter] = React.useState(0)
+  const [score, setScore] = React.useState(false)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  function handleClick(isTrue)
+  {
+    if(isTrue)
+    {
+      setCounter(counter+1)
+    }
+    const nextQues = currQues + 1
+    if(nextQues<mySet.length)
+    {
+      setQues(nextQues)
+    }
+    else
+    {
+      setScore(true)
+    }
+  }
+
+  const myData = mySet[currQues].ans.map((item) =>{
+    return (<button onClick={(()=>handleClick(item.isTrue))}>{item.answers}</button>)
+  })
+  return(
+  <div className="container">
+  {score ?
+    (<div className="result-section">You scored {counter} out of {mySet.length}
+    </div>) : <><div className="ques-section">
+      <div className="ques-number"><span>Question {currQues}</span>/{mySet.length}</div>
+      <div className="question">{mySet[currQues].ques}</div>
     </div>
-  );
+    <div className="ans-section">
+      {myData}
+    </div>  </>
+ } 
+  </div>)
 }
-
-export default App;
---
+++ src/Data.js
@@ -0,0 +1,61 @@
+export default[
+    {ques: "India's largest city by population",
+    ans:[
+     {answers: "Delhi", isTrue: false},
+     {answers: "Chennai", isTrue: false},
+     {answers: "Mumbai", isTrue: true},
+     {answers: "Pune", isTrue: false},
+]},
+
+     {ques: "India's total area",
+    ans:[
+     {answers: "3.7 Million sq. km", isTrue: false},
+     {answers: "3.0 Million sq. km", isTrue: false},
+     {answers: "3.3 Million sq. km", isTrue: true},
+     {answers: "4.2 Million sq. km", isTrue: false},
+]},
+    
+     {ques: "National Start-up Day will be celebrated every year on",
+     ans:[
+      {answers: "16th January", isTrue: true},
+      {answers: "12th January", isTrue: false},
+      {answers: "20th January", isTrue: false},
+      {answers: "24th January", isTrue: false},
+    ]},
+      
+      {ques: "Who is India's first Woman Rafale pilot",
+    ans:[
+     {answers: "Shivangi Joshi", isTrue: false},
+     {answers: "Avani Chaturvedi", isTrue: false},
+     {answers: "Bhawana Kanth", isTrue: false},
+     {answers: "Shivangi Singh", isTrue: true},
+]},
+     {ques: "Major Dhyan Chand Sports University will be established in",
+    ans:[
+     {answers: "Lucknow", isTrue: false},
+     {answers: "Bareilly", isTrue: false},
+     {answers: "Meerut", isTrue: true},
+     {answers: "Varanasi", isTrue: false},
+]},
+     {ques: "Who was become the first woman in the world to reach 300 million followers on Instagram",
+    ans:[
+     {answers: "Kendall Jenner", isTrue: false},
+     {answers: "Kylie Jenner", isTrue: true},
+     {answers: "Megon Foxx", isTrue: false},
+     {answers: "Gigi Hdid", isTrue: false},
+]},
+     {ques: "Novak Djokovic was not allowed to participate in which tournament as he was not covid vaccinated",
+    ans:[
+     {answers: "US Open", isTrue: false},
+     {answers: "Australian Open", isTrue: true},
+     {answers: "France Open", isTrue: false},
+     {answers: "Wimbledon", isTrue: false},
+]},
+     {ques: "India's first export of BrahMos cruise missile is scheduled with which country",
+    ans:[
+     {answers: "Philippines", isTrue: true},
+     {answers: "Malaysia", isTrue: false},
+     {answers: "Vietnam", isTrue: false},
+     {answers: "Indonesia", isTrue: false},
+]},
+    ]
